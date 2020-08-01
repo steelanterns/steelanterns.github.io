96439c7b5e71a6ffe67e4a00bf5d023eb4d56a5a
steelanterns <steevejeanchilles@gmail.com>
2020-08-01 15:10:50 -0400

    Updates

---
+++ precache-manifest.6e7c0480cc9980f90e12530be115a556.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a944df8b24323b352eea2a852cf66df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e8289228c731f2b28e",
+    "url": "/static/css/main.ec13d1ed.chunk.css"
+  },
+  {
+    "revision": "91f24918bb9a29bded0e",
+    "url": "/static/js/2.f8562c7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f8562c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e8289228c731f2b28e",
+    "url": "/static/js/main.d0e3e83b.chunk.js"
+  },
+  {
+    "revision": "5b99612e902809d849b7",
+    "url": "/static/js/runtime-main.6688b816.js"
+  },
+  {
+    "revision": "8d7072c9b683a2ae3c7168bb83e34ba8",
+    "url": "/static/media/fmn.8d7072c9.png"
+  },
+  {
+    "revision": "9ccd52b02a5549f857a4483067ea4676",
+    "url": "/static/media/jumbotron.9ccd52b0.png"
+  },
+  {
+    "revision": "e17c3cc138b4368782c4c663793d4d36",
+    "url": "/static/media/software-development.e17c3cc1.png"
+  },
+  {
+    "revision": "e6f1a21d0aef306b7b2913e9a6df9da2",
+    "url": "/static/media/web-dev.e6f1a21d.png"
+  },
+  {
+    "revision": "7ecdc35b00e09dcb88ad304e38972a19",
+    "url": "/static/media/webdevp.7ecdc35b.png"
+  }
+]);
